d7237e0b445552d9239cba9333a9875d5e70af01
ChiefMaarten <68859524+ChiefMaarten@users.noreply.github.com>
2022-05-23 09:26:12 +0200

    Update with aero

--- RunModel.m
+++ RunModel.m
@@ -1,11 +1,16 @@
 %% Parameter script
-par.m_s = 200;
-par.m_u = 10;
+par.m_s = 40;
+par.m_u = 8;
 par.g = 9.81;
-par.z0 = 100; % mm
-par.K_s = 1000;
-par.K_t = 1000;
-par.C_s = 1000;
-par.C_t = 1000;
+par.z0 = 0.100; % m
+par.K_s = 2000; % N/m
+par.K_t = 15000; % N/m
+par.C_s = 1200; 
+par.C_t = 0.2;
 
-run QCM
+% downforce
+par.initial_downforce = 1000;
+par.delay = 2; % delay in transient 
+
+%%
+sim QCM
