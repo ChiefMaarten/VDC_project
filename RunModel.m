b373396a87d1a094ce16069d31ac44e6a89d880b
ChiefMaarten <68859524+ChiefMaarten@users.noreply.github.com>
2022-05-23 12:04:11 +0200

    Merged model

--- RunModel.m
+++ RunModel.m
@@ -2,7 +2,7 @@
 par.m_s = 40;
 par.m_u = 8;
 par.g = 9.81;
-par.z0 = 0.100; % m
+par.z0 = 0.200; % m
 par.K_s = 2000; % N/m
 par.K_t = 15000; % N/m
 par.C_s = 1200; 
@@ -11,6 +11,16 @@ par.C_t = 0.2;
 % downforce
 par.initial_downforce = 1000;
 par.delay = 2; % delay in transient 
+par.rho =1.225;
+par.A = 1.5;
+par.Cl = 3.7;
+par.Cd = 1.0;
+par.V = 80;
+
+par.floorlength = 2;
+par.ainlet = 0.03;
+par.floorwidth = 0.5;
+par.pstatic = 101325;
 
 %%
 sim QCM
