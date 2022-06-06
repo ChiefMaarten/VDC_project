94c2d311dcb53e9d628c0105225e9db8618d1145
ChiefMaarten <68859524+ChiefMaarten@users.noreply.github.com>
2022-06-06 11:58:16 +0200

    Update

--- RunModel.m
+++ RunModel.m
@@ -1,13 +1,17 @@
 %% Parameter script
-% QC parameters
-par.m_s = 40;
-par.m_u = 8;
+par.m_s = 207;
+par.m_u = 20;
+par.m = 4*(par.m_u + par.m_s);
 par.g = 9.81;
 par.z0 = 0.200; % m
-par.K_s = 2000; % Suspension stiffness N/m
-par.K_t = 15000; % Tyre stifness N/m
-par.C_s = 1200; % Suspension damping
-par.C_t = 0.2;
+par.K_t = 6E3 / 25E-3; % N/m
+par.K_s = par.K_t * 4; % N/m
+f_eigen = 4; % Hz
+D = 0.7; % damping ratio [-]
+k = par.K_t*par.K_s/(par.K_t + par.K_s); % effective stiffness for sprung mass
+c_crit = 2*sqrt(k*par.m_s);
+par.C_s = D*c_crit; 
+par.C_t = 450;
 
 par.wn_s = sqrt(par.K_s*par.K_t / (par.K_s + par.K_t)/par.m_s);              % sprung mass natural frequency
 par.d_s = 0.3 * (2 * par.m_s * par.wn_s);                        % damping ratio
@@ -25,6 +29,10 @@ par.floorlength = 2;
 par.ainlet = 0.03;
 par.floorwidth = 0.5;
 par.pstatic = 101325;
+
+% simulation lon vel
+par.v0 = 30;
+par.F_engine = 7.2593e+03; % total engine force on tires
 
 %% LQR controller (partially copied from RO47017 Vehicle Dynamics & Control, 2022)
 % state matrix
